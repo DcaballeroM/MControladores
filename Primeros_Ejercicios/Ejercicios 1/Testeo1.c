5570bba34e7e1819fe22cceb9487b70875a33447
Daniel <ichbincaba@gmail.c>
2025-08-22 21:47:55 -0500

    Primer commit en rama Testeo

---
+++ Primeros_Ejercicios/Ejercicios 1/Testeo1.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+    int num1 = 8;   // primer número definido
+    int num2 = 5;   // segundo número definido
+    int suma;
+
+    suma = num1 + num2;
+
+    printf("La suma de %d + %d es: %d\n", num1, num2, suma);
+
+    return 0;
+}
+
+
+
+
